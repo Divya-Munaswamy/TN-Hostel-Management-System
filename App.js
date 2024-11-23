6e32cc0390e7578fac0b038c94ac821b39cd6409
Divya-Munaswamy <110138698+Divya-Munaswamy@users.noreply.github.com>
2024-11-23 14:27:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import Main from './hostelmanagementsys/Main';
+import Main from './Component/Main';
+
+
+//import Main from './Component2/Main';
+
+
+function App() {
+
+  return (
+  <>
+<Main/>
+
+  </>
+
+  
+  );
+}
+
+
+
+export default App
